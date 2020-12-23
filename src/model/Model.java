0eacb5e3b866be4ba1355c17dd242cd5a6e1b3a1
Arthur Arnx <arthur.arnx@gmail.com>
2020-12-23 11:01:27 +0100

    Initial commit

---
+++ src/model/Model.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+
+public class Model {
+    private List<PrivateKey> keys;
+    private KeyStore ks;
+
+
+}
