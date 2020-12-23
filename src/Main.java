7d262cd962997ba6d479c674f1ea9242e69d2211
Arthur Arnx <arthur.arnx@gmail.com>
2020-12-23 14:27:00 +0100

    Generate this.certs

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import model.Model;
 
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
     private static Model model;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         model = new Model();
         model.populate();
         model.test();
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,12 +1,13 @@
 package model;
 
+import java.security.*;
 import java.util.List;
-import java.security.KeyStore;
-import java.security.Principal;
-import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.*;
+import java.math.BigInteger;
 
 public class Model {
 
@@ -14,9 +15,42 @@ public class Model {
     private List<PrivateKey> keys;
     private List<KeyStore> ks;
 
-    public void populate(){
-        // TODO: 1_Generate this.certs using CertificateBuilder and CertificateHandler
+    public BigInteger generateId() {
+        BigInteger counter = BigInteger.ONE;
+        counter = counter.add(BigInteger.ONE);
+        return counter;
+    }
+
+    public void populate() throws NoSuchAlgorithmException {
         System.out.println("populate");
+
+        // Le DN du CA
+        final String DN = "CN=RootCA, OU=M2, O=miage, L=Mulhouse, ST=68093, C=FR";
+        X500Name caDn = new X500Name(DN);
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair caKp = kpg.generateKeyPair();
+        byte[] encodedPbKey = caKp.getPublic().getEncoded();
+        SubjectPublicKeyInfo subjectPublicKeyInfo = SubjectPublicKeyInfo.getInstance(encodedPbKey);
+
+        // TODO: 1_Generate this.certs using CertificateBuilder and CertificateHandler
+        this.certs = new X509v3CertificateBuilder(
+                // nom de l'émetteur
+                caDn,
+                // numéro de série du certificat
+                generateId(),
+                // début de la période de validité
+                new Date(System.currentTimeMillis()),
+                // fin de la période de validité
+                new Date(System.currentTimeMillis() + (long) 365 * 24 * 60 * 60 * 1000),
+                // le nom du sujet
+                caDn,
+                // la clé publique enveloppée dans le certificat
+                subjectPublicKeyInfo
+        );
+
+
         X509v3CertificateBuilder v3CertGen;
 
         // TODO: 2_Generate this.keys
