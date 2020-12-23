ef5427eebda98a7b6c6523622756b58e55d3e900
TruiteAgitee <pelegrin.gregoire@gmail.com>
2020-12-23 12:01:10 +0100

    Add libraries

--- src/model/Model.java
+++ src/model/Model.java
@@ -2,11 +2,26 @@ package model;
 
 import java.util.List;
 import java.security.KeyStore;
+import java.security.Principal;
 import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.cert.*;
 
 public class Model {
+
+    private List<X509Certificate> certs;
     private List<PrivateKey> keys;
-    private KeyStore ks;
+    private List<KeyStore> ks;
 
+    private void test(){
+        X509Certificate c = certs.get(0);
+        Principal p1 = c.getSubjectDN();
+        Principal p2 = c.getIssuerDN();
+        System.out.println("Subject: " + p1 + "\nIssuer: " + p2);
+    }
 
+    public void populate(){
+        X509v3CertificateBuilder v3CertGen;
+    }
 }
