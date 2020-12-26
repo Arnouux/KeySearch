ccc796cc103878b7321d06f96909f08745df7673
Arthur Arnx <arthur.arnx@gmail.com>
2020-12-26 10:52:05 +0100

    file chooser

--- src/ui/App.java
+++ src/ui/App.java
@@ -2,9 +2,7 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class App extends JFrame {
 
@@ -17,8 +15,13 @@ public class App extends JFrame {
     JScrollPane jspName = new JScrollPane(input);
 
     // Certificate
-    JFileChooser fileChooser = new JFileChooser();
-    JScrollPane jspCertificate = new JScrollPane(fileChooser);
+    JFileChooser fileChooserCertificate = new JFileChooser(System.getProperty("user.dir"));
+    JFileChooser fileChooserKeys = new JFileChooser(System.getProperty("user.dir"));
+    JButton buttonOpenFileChooserCertificate = new JButton("Certificate");
+    JButton buttonOpenFileChooserKeys = new JButton("Keys file");
+    JLabel fileCertificateName = new JLabel("");
+    JLabel fileKeysName = new JLabel("");
+    JPanel btnPanel = new JPanel(new GridLayout(3,2, 2, 2));
 
     public App() {
         super("KeySearch");
@@ -32,6 +35,20 @@ public class App extends JFrame {
 
 
         });
+
+        buttonOpenFileChooserCertificate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showFileChooser("Certificate");
+            }
+        });
+        buttonOpenFileChooserKeys.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showFileChooser("Keys");
+            }
+        });
+
         this.setPreferredSize(new Dimension(600, 600));
 
         this.setLayout(new BorderLayout());
@@ -84,7 +101,9 @@ public class App extends JFrame {
     private void updatePanels(String value) {
         this.remove(jspKey);
         this.remove(jspName);
-        this.remove(jspCertificate);
+        this.remove(buttonOpenFileChooserKeys);
+        this.remove(buttonOpenFileChooserCertificate);
+        this.remove(btnPanel);
 
         switch(value) {
             case "Key":
@@ -99,12 +118,39 @@ public class App extends JFrame {
                 break;
             case "Certificate":
                 System.out.println("Certificate");
-                this.add(jspCertificate, BorderLayout.SOUTH);
-                this.setPreferredSize(new Dimension(600, 430));
+                //this.add(buttonOpenFileChooserCertificate);
+                //this.add(buttonOpenFileChooserKeys);
+
+                btnPanel.add(buttonOpenFileChooserCertificate);
+
+                btnPanel.add(fileCertificateName);
+
+                btnPanel.add(buttonOpenFileChooserKeys);
+
+                btnPanel.add(fileKeysName);
+
+                this.add(btnPanel, BorderLayout.SOUTH);
+                //fileChooserCertificate.showOpenDialog(this);
+                this.setPreferredSize(new Dimension(600, 230));
                 break;
             default:break;
         }
         this.pack();
     }
+
+    private void showFileChooser(String value) {
+        switch(value) {
+            case "Certificate":
+                fileChooserCertificate.showOpenDialog(this);
+                this.fileCertificateName.setText(fileChooserCertificate.getSelectedFile().getName());
+                break;
+            case "Keys":
+                fileChooserKeys.showOpenDialog(this);
+                this.fileKeysName.setText(fileChooserKeys.getSelectedFile().getName());
+                break;
+            default:break;
+        }
+    }
+
 }
 
